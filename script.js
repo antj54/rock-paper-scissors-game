f14ecdd9de78aa2a636bb635ffbb6248a9f4add0
antj54 <anthonyjimenez592@gmail.com>
2022-08-13 16:29:45 -0400

    Added updating html when JS function is called

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.getElementById('rock').onclick = pick;
 document.getElementById('paper').onclick = pick;
 document.getElementById('scissors').onclick = pick;
-
+let userScore = 0;
+let computerScore = 0;
+let ties = 0;
 
 function pick() {
+    //picks user choice
     let userChoice = this.id;
     console.log(userChoice);
     
-
+    //picks computer choice
     let computerChoice = Math.random();
 
     if (computerChoice < 0.34){
@@ -19,36 +22,52 @@ function pick() {
     }
 
     console.log(computerChoice)
-    
+    document.getElementById('user-pick').innerHTML = "User picks: " + userChoice;
+    document.getElementById('computer-pick').innerHTML = "Computer picks: " + computerChoice;
+    //compares choices
+        if (userChoice === computerChoice){
+            ties++;
+            document.getElementById("ties").innerHTML = "Ties: " + ties; 
+            return "The result is a tie"
+            }
 
-    function comparePicks(userChoice, computerChoice) {
-        if (userChoice === computerChoice) {
-            return "The result is a tie!"
-        }
-    }
         if (userChoice === "rock") {
         if (computerChoice === "scissors") {
-            return"player wins";
+            userScore++;
+            document.getElementById('player-score').innerHTML = "Player Score: " + userScore;
+            return "player wins";
         }else{
+            computerScore++;
+            document.getElementById('computer-score').innerHTML = "Computer Score: " + computerScore;
             return "computer wins";
         }
             }
 
         if (userChoice === "paper") {
          if (computerChoice === "rock") {
+            userScore++;
+            document.getElementById('player-score').innerHTML = "Player Score: " + userScore;
              return "player wins";
           } else {
+            computerScore++;
+            document.getElementById('computer-score').innerHTML = "Computer Score: " + computerScore;
               return "computer wins";
           }
          }
     
          if (userChoice === "scissors") {
          if (computerChoice === "rock") {
+            computerScore++;
+            document.getElementById('computer-score').innerHTML = "Computer Score: " + computerScore;
              return "computer wins";
          } else {
+            userScore++;
+            document.getElementById('player-score').innerHTML = "Player Score: " + userScore;
              return "player wins";
         }
-}
+         
+};
+
 }
 
 
